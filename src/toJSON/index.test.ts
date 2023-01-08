888288e1d151be665ca1ce75191eeea56dbad8a8
James Arthur <jarthur96@hotmail.com>
2023-01-08 21:28:34 +1300

    Moving test

--- src/toJSON/index.test.ts
+++ src/toJSON/index.test.ts
@@ -1,13 +1,13 @@
 import * as assert from 'assert';
 
-import { createObjectFromString } from './toJSON';
+import { createObjectFromString } from '.';
 
 const jsObjectString = `{
   a: 1,
   b: '2',
   c: [3, 4, 5],
   d: {
-    e: 6,
+    e: true,
     f: '2023-01-01T00:00:00Z',
   }
 }`;
@@ -17,12 +17,12 @@ const jsObject = {
   b: '2',
   c: [3, 4, 5],
   d: {
-    e: 6,
+    e: true,
     f: '2023-01-01T00:00:00Z',
   },
 };
 
-suite('jsonify', () => {
+suite('toJSON', () => {
   suite('createObjectFromString', () => {
     test('should succesfully convert JS object to JSON', () => {
       assert.equal(createObjectFromString(jsObjectString), jsObject);
